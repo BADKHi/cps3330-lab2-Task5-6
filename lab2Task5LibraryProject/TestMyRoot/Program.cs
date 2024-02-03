a35600dd942f159d9ecb40c87713e3f384287783
Mekhi Wood-Brock <106398956+BADKHi@users.noreply.github.com>
2024-02-03 12:51:08 -0500

    Add files via upload

---
+++ lab2Task5LibraryProject/TestMyRoot/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using lab2Task5Library;
+
+    namespace TestMyRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1, num2, num3, max, val;
+
+            Console.WriteLine("Do you want to find the MAXIMUM value between 2 or 3 numbers? ");
+        while (true)
+            {
+                val = Convert.ToDouble(Console.ReadLine());
+
+                if (val == 2)
+                {
+                    Console.WriteLine("Lets find the MAXIMUM value between two numbers!");
+                    Console.Write("Enter your first number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter your second number: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+
+                    max = lab2Task5Library.Class1.Max2Num(num1, num2);
+                }
+                else if (val == 3)
+                {
+                    Console.WriteLine("Lets find the MAXIMUM value between three numbers!");
+                    Console.Write("Enter your first number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter your second number: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter your third number: ");
+                    num3 = Convert.ToDouble(Console.ReadLine());
+
+                    max = lab2Task5Library.Class1.Max3Num(num1, num2, num3);
+                }
+                else
+                    Console.Write("Please enter the number 2 or 3: ");
+
+            }
+            //Mekhi Wood-Brock
+            //CPS3330
+            //Section *01
+            //Lab #2
+
+
+        }
+
+    }
+}
+
---
+++ lab2Task5LibraryProject/lab2Task5Library/Class1.cs
@@ -0,0 +1,38 @@
+﻿namespace lab2Task5Library;
+
+public class Class1
+{
+    public static double Max2Num(double number1, double number2)
+    {
+        double max;
+        if (number1 > number2)
+            max = number1;
+        else
+            max = number2;
+
+        Console.WriteLine($"The MAX value between {number1} and {number2} is {max}");
+        return max;
+
+    }
+
+    public static double Max3Num(double number1, double number2, double number3)
+    {
+        double max;
+        if (number1 > number2 && number1 > number3)
+            max = number1;
+        else if (number2 > number1 && number2 > number3)
+            max = number2;
+        else
+            max = number3;
+
+        Console.WriteLine($"The MAX value between {number1}, {number2}, and {number3} is {max}");
+        return max;
+    }
+    //Mekhi Wood-Brock
+    //CPS3330
+    //Section *01
+    //Lab #2
+
+}
+
+
